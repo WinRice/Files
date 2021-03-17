75a2dd2139662108f9dc7d8aaac2ebcbfb80afb2
Pratyaksh Mehrotra <pratyakshm@protonmail.com>
2021-03-17 11:17:44 +0530

    connect.cmd: supress output and exit when done

--- connect.cmd
+++ connect.cmd
@@ -1,9 +1,7 @@
 @echo off
 cd /d "%~dp0"
-echo Uninstalling the Connect app...
 CLS
 install_wim_tweak.exe /o /l
 install_wim_tweak.exe /o /c  Microsoft-PPIProjection-Package /r
 install_wim_tweak.exe /h /o /l
-echo The Connect app should be uninstalled. Please reboot Windows 10.
-pause
+exit
